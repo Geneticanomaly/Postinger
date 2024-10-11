17ff1c8d9b7b50c9245c343781eb3a09a665150e
Geneticanomaly <thomaskaatranen@gmail.com>
2024-10-11 17:32:04 +0300

    Configured a simple development environment with docker.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Postinger
+
+Starting dev environment
+
+-   docker build -f ./dev.Dockerfile -t postinger-dev-postgres .
+-   docker compose -f docker-compose.dev.yml up
+
+Access database through the command line
+
+-   docker exec -it "containerId" psql -U the_username the_database
--- server/util/db.ts
+++ server/util/db.ts
@@ -3,11 +3,13 @@ import { Umzug, SequelizeStorage } from 'umzug';
 import { DATABASE_URL } from './config';
 import path from 'path';
 
+// Initialize Sequelize with Sequelize-typescript
 const sequelize = new Sequelize(DATABASE_URL, {
     dialect: 'postgres',
     models: [path.resolve(__dirname, 'models')],
 });
 
+// Function to run migrations
 const runMigrations = async () => {
     const migrator = new Umzug({
         migrations: {
@@ -23,6 +25,7 @@ const runMigrations = async () => {
     });
 };
 
+// Function to connect to the database and run migrations
 const connectToDatabase = async (): Promise<void> => {
     try {
         await sequelize.authenticate();
