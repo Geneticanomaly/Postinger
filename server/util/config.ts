3e846869202930745b2c921d8f9a511484d1aeb1
Geneticanomaly <thomaskaatranen@gmail.com>
2024-10-11 17:58:35 +0300

    Made some slight changes so the server goes up as well with the PostgreSQL db when the docker docker-compose.dev.yml file built.

--- server/util/config.ts
+++ server/util/config.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const DATABASE_URL: string = process.env.DATABASE_URL || '';
+const DATABASE_DEV_URL: string =
+    process.env.DATABASE_DEV_URL || 'postgres://the_username:the_password@localhost:5432/the_database';
+
+export const DATABASE_URL: string = process.env.DATABASE_URL || DATABASE_DEV_URL;
 export const PORT: number = Number(process.env.PORT) || 3003;
 export const SECRET: string = process.env.SECRET || '';
