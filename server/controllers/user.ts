fdce67071c1bf6c01ffda5b8cee85961f501bc3a
Geneticanomaly <thomaskaatranen@gmail.com>
2024-11-13 14:14:00 +0200

    Added updateUserImage route and controller.

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,8 +1,39 @@
 import { Request, Response } from 'express';
 import User from '../database/models/user';
+import File from '../database/models/file';
+import multer from 'multer';
 require('express-async-errors');
 
 export const getUsers = async (_req: Request, res: Response) => {
     const users = await User.findAll();
     res.json(users);
+};
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const uploadMiddleware = upload.single('file');
+
+export const updateUserImage = async (req: Request<{ id: string }, {}, {}>, res: Response) => {
+    uploadMiddleware(req, res, async (err: any) => {
+        if (err) {
+            res.status(500).json({ error: 'File upload failed' });
+        }
+        const user = await User.findByPk(req.params.id);
+
+        if (req.file && user) {
+            const newFile = {
+                userId: user.id,
+                name: req.file.originalname,
+                encoding: req.file.encoding,
+                mimetype: req.file.mimetype,
+                buffer: req.file.buffer,
+                fileType: 'profile',
+            };
+
+            await File.create(newFile);
+        }
+
+        res.json(user);
+    });
 };
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { getUsers } from '../controllers/user';
+import { getUsers, updateUserImage } from '../controllers/user';
 
 export const router: Router = Router();
 
 router.get('/v1/users/', getUsers);
+router.post('/v1/users/:id', updateUserImage);
