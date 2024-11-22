b337805f0afb640d7045be8fe8affc182d0f786c
Geneticanomaly <thomaskaatranen@gmail.com>
2024-11-22 16:55:37 +0200

    If user's refresh has expired navigate to login.

--- client/src/hooks/useGetCurrentUser.ts
+++ client/src/hooks/useGetCurrentUser.ts
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { useUserDispatch, useUserValue } from '../context/userContext/useUserContext';
 import userServices from '../services/user';
+import { useNavigate } from 'react-router-dom';
 
 export const useGetCurrentUser = () => {
+    const navigate = useNavigate();
     const user = useUserValue();
     const userDispatch = useUserDispatch();
 
     useEffect(() => {
         const getCurrentUser = async () => {
-            const current_user = await userServices.getCurrentUser();
-
-            if (current_user) {
-                userDispatch({ type: 'SET', payload: current_user });
+            try {
+                const res = await userServices.getCurrentUser();
+                userDispatch({ type: 'SET', payload: res });
+            } catch {
+                navigate('/login');
             }
         };
         if (!user) getCurrentUser();
-    }, [user, userDispatch]);
+    }, [navigate, user, userDispatch]);
 };
